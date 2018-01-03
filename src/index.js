55b3da3dcd8bf67e5a25c3c39c40f141681a0c16
Binh Khuu <binhkhuu90@gmail.com>
2018-01-03 16:05:02 +0800

    designed bord added Css styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Squares extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			value: 'X'
+		}
+	}
+	handleClick(i) {
+		console.log(i +' clicked');
+		var value = (this.state.value === 'X') ? 'O' : 'X';
+		this.setState({value: value});
+	}
+	render () {
+		return (
+			<div className='square' onClick={() => this.handleClick(this.props.num) }><h1>{this.state.value}</h1></div>
+		)
+	}
+}
+
+class Board extends React.Component {
+	render () {
+		var squares = Array(9).fill(0);
+		console.log(squares);
+		return (			
+			<div className='container-fluid background'>
+				<div className='board'>
+				{squares.map((x,i)=>{
+					return <Squares key={'square-'+ i} num={i}/>
+				})}
+				</div>
+			</div>
+		)
+	}
+
+}
+
+ReactDOM.render(
+  <Board />,
+    document.getElementById('root')
+);
